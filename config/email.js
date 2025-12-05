d19aaafdb565ab232004ea6d15262a3ba0354697
nisma-banu <nismabanu012@gmail.com>
2025-12-05 21:15:59 +0530

    mail changes

--- config/email.js
+++ config/email.js
@@ -9,6 +9,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER, // example: 9d64dc001@smtp-brevo.com
     pass: process.env.SMTP_PASS, // your Brevo SMTP key
   },
+   tls: {
+    ciphers: "SSLv3",
+    rejectUnauthorized: false, // important for Render hosting
+  },
 });
 
 module.exports = transporter;
