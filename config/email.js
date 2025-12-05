7f18217913f94e98c5cbe5f37c27e2a48904daa8
Nisma-feather <shanthinifeathers16@gmail.com>
2025-12-05 17:28:29 +0530

    Initial Commit

--- config/email.js
+++ config/email.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
-  secure: false,
+  secure: false, 
   auth: {
     user: process.env.SMTP_USER, // example: 9d64dc001@smtp-brevo.com
     pass: process.env.SMTP_PASS, // your Brevo SMTP key
