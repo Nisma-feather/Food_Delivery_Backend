d678ffc4cfff8895b5049368c8453f7e16d4d6d7
nisma-banu <nismabanu012@gmail.com>
2025-12-05 21:32:55 +0530

    mail changes

--- config/email.js
+++ config/email.js
@@ -2,16 +2,15 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false, 
+  requireTLS:true,
+  logger:true,
+  debug:true,
   auth: {
-    user: process.env.SMTP_USER, // example: 9d64dc001@smtp-brevo.com
-    pass: process.env.SMTP_PASS, // your Brevo SMTP key
-  },
-   tls: {
-    ciphers: "SSLv3",
-    rejectUnauthorized: false, // important for Render hosting
+    user: process.env.EMAIL_USER, // example: 9d64dc001@smtp-brevo.com
+    pass: process.env.EMAIL_PASS, // your Brevo SMTP key
   },
 });
 
