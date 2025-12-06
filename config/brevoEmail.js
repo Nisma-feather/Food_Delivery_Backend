7da079e7fc728bd2178c5faf85b083e7de80817d
Nisma-feather <shanthinifeathers16@gmail.com>
2025-12-06 12:51:00 +0530

    using brevo api

--- config/brevoEmail.js
+++ config/brevoEmail.js
@@ -2,7 +2,7 @@ const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const apiKey = defaultClient.authentications["api-key"];
-apiKey.apiKey = process.env.SMTP_PASS;
+apiKey.apiKey = process.env.BREVO_API_KEY;
 const brevo = new SibApiV3Sdk.TransactionalEmailsApi();
 
 module.exports = brevo;
