ca308453d536732a7d6f70cd7b121e4719cff95a
Nisma-feather <shanthinifeathers16@gmail.com>
2025-12-06 10:31:56 +0530

    using brevo api

---
+++ controllers/testController.js
@@ -0,0 +1,25 @@
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = process.env.BREVO_PASS; // Your API key
+
+const transEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+
+exports.sendEmail = async (req, res) => {
+  try {
+    const result = await transEmailApi.sendTransacEmail({
+      sender: { email: "shanthinifeathers16.com", name: "Delivery App" },
+      to: [{ email: req.body.email }],
+
+      subject: "Your OTP Code",
+      htmlContent: `<h1>Test Email worked successsfully</h1>`,
+    });
+
+    console.log("Email sent:", result);
+    res.json({ success: true, message: "Email sent!" });
+  } catch (error) {
+    console.error("Brevo API Error:", error);
+    res.status(500).json({ success: false, error });
+  }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,13 @@ const express= require('express');
 const router = express.Router();
 
 const {sendOtp,verifyOtp,sendForgotOtp,verifyForgotOtp,resetPassword} = require("../controllers/otpController");
+const {sendEmail} = require("../controllers/testController")
+
+
 
 router.post("/send-otp",sendOtp);
 router.post("/verify-otp",verifyOtp);
+router.post("/test-otp", sendEmail);
 router.post("/forgot/send-otp", sendForgotOtp);
 router.post("/forgot/verify-otp", verifyForgotOtp);
 router.post("/forgot/reset-password", resetPassword);
